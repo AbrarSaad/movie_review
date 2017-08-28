e8e3ab16b139bc6099d35d7eb1ce7797279f5435
Abrar Saad <wifibaghdad@gmail.com>
2017-08-28 17:39:52 +0100

    compleat 5  star

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,16 +1,7 @@
 class ReviewsController < ApplicationController
   before_action :set_review, only: [:show, :edit, :update, :destroy]
+  before_action :set_movie
   before_action :authenticate_user!
-  # GET /reviews
-  # GET /reviews.json
-  def index
-    @reviews = Review.all
-  end
-
-  # GET /reviews/1
-  # GET /reviews/1.json
-  def show
-  end
 
   # GET /reviews/new
   def new
@@ -26,16 +17,15 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @review.user_id = current_user.id
-    respond_to do |format|
+    @review.movie_id = @movie.id
+
       if @review.save
-        format.html { redirect_to @review, notice: 'Review was successfully created.' }
-        format.json { render :show, status: :created, location: @review }
+      redirect_to @movie
       else
-        format.html { render :new }
-        format.json { render json: @review.errors, status: :unprocessable_entity }
+      render 'new'
       end
     end
-  end
+
 
   # PATCH/PUT /reviews/1
   # PATCH/PUT /reviews/1.json
@@ -65,6 +55,10 @@ class ReviewsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_review
       @review = Review.find(params[:id])
+    end
+
+    def set_movie
+      @movie = Movie.find(params[:movie_id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :reviews
+
+  post '/rate' => 'rater#create', :as => 'rate'
   devise_for :users
-  resources :movies
+  resources :movies do
+  resources :reviews, except: [:show, :index]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'movies#index'
 end
